afd16108828db388bb67942b5509d578c0ed5370
Oleg Glukhov <o.gluhov2017@yandex.ru>
2024-01-22 11:23:20 +0300

    Add app tetris

---
+++ src/game.js
@@ -0,0 +1,292 @@
+export default class Game {
+	static points = {
+		1: 40,
+		2: 100,
+		3: 300,
+		4: 1200,
+	}
+	score = 0
+	lines = 0
+	topOut = false
+	playfield = this.createPlayfield()
+	activePiece = this.createPiece()
+	nextPiece = this.createPiece()
+	constructor() {
+		this.reset()
+	}
+
+	get level() {
+		return Math.floor(this.lines * 0.1)
+	}
+
+	getState() {
+		const playfield = this.createPlayfield()
+		const { y: pieceY, x: pieceX, blocks } = this.activePiece
+
+		for (let y = 0; y < this.playfield.length; y++) {
+			playfield[y] = []
+
+			for (let x = 0; x < this.playfield[y].length; x++) {
+				playfield[y][x] = this.playfield[y][x]
+			}
+		}
+
+		for (let y = 0; y < blocks.length; y++) {
+			for (let x = 0; x < blocks[y].length; x++) {
+				if (blocks[y][x]) {
+					playfield[pieceY + y][pieceX + x] = blocks[y][x]
+				}
+			}
+		}
+
+		return {
+			score: this.score,
+			level: this.level,
+			lines: this.lines,
+			nextPiece: this.nextPiece,
+			playfield,
+			isGameOver: this.topOut,
+		}
+	}
+	
+	reset() {
+		this.score = 0
+		this.lines = 0
+		this.topOut = false
+		this.playfield = this.createPlayfield()
+		this.activePiece = this.createPiece()
+		this.nextPiece = this.createPiece()
+	}
+
+	createPlayfield() {
+		const playfield = []
+
+		for (let y = 0; y < 20; y++) {
+			playfield[y] = []
+
+			for (let x = 0; x < 10; x++) {
+				playfield[y][x] = 0
+			}
+		}
+
+		return playfield
+	}
+
+	createPiece() {
+		const y = Math.floor(Math.random() * 7)
+		const type = 'IJLOSTZ'[y]
+		const piece = {}
+
+		switch (type) {
+			case 'I':
+				piece.blocks = [
+					[0, 0, 0, 0],
+					[1, 1, 1, 1],
+					[0, 0, 0, 0],
+					[0, 0, 0, 0],
+				]
+				break
+			case 'J':
+				piece.blocks = [
+					[0, 0, 0],
+					[2, 2, 2],
+					[0, 0, 2],
+				]
+				break
+			case 'L':
+				piece.blocks = [
+					[0, 0, 0],
+					[3, 3, 3],
+					[0, 0, 0],
+				]
+				break
+			case 'O':
+				piece.blocks = [
+					[0, 0, 0, 0],
+					[0, 4, 4, 0],
+					[0, 4, 4, 0],
+					[0, 0, 0, 0],
+				]
+				break
+			case 'S':
+				piece.blocks = [
+					[0, 0, 0],
+					[0, 5, 5],
+					[5, 5, 0],
+				]
+				break
+			case 'T':
+				piece.blocks = [
+					[0, 0, 0],
+					[6, 6, 6],
+					[0, 6, 0],
+				]
+				break
+			case 'Z':
+				piece.blocks = [
+					[0, 0, 0],
+					[7, 7, 0],
+					[0, 7, 7],
+				]
+				break
+			default:
+				throw new Error('Неизвестный тип фигуры')
+		}
+		piece.x = Math.floor((10 - piece.blocks[0].length) / 2)
+		piece.y = -1
+		return piece
+	}
+
+	movePieceLeft() {
+		this.activePiece.x -= 1
+		if (this.isPieceOutOfBounds()) {
+			this.activePiece.x += 1
+		}
+	}
+
+	movePieceRight() {
+		this.activePiece.x += 1
+		if (this.isPieceOutOfBounds()) {
+			this.activePiece.x -= 1
+		}
+	}
+
+	movePieceDown() {
+		if (this.topOut) return
+
+		this.activePiece.y += 1
+
+		if (this.isPieceOutOfBounds()) {
+			this.activePiece.y -= 1
+			this.lockPiece()
+			const clearLines = this.clearLines()
+			this.updateScore(clearLines)
+			this.updatePieces()
+		}
+
+		if (this.hasCollision()) {
+			this.topOut = true
+		}
+	}
+
+	isPieceOutOfBounds() {
+		const { y: pieceY, x: pieceX, blocks } = this.activePiece
+		const playfield = this.playfield
+
+		for (let y = 0; y < blocks.length; y++) {
+			for (let x = 0; x < blocks[y].length; x++) {
+				if (
+					blocks[y][x] &&
+					(playfield[pieceY + y] === undefined ||
+						playfield[pieceY + y][pieceX + x] === undefined ||
+						playfield[pieceY + y][pieceX + x])
+				) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+	rotatePiece() {
+		this.rotateBlocks()
+		if (this.hasCollision()) {
+			this.rotateBlocks(false)
+		}
+	}
+
+	rotateBlocks(clockWise = true) {
+		const blocks = this.activePiece.blocks
+		const length = blocks.length
+		const x = Math.floor(length / 2)
+		const y = length - 1
+
+		for (let i = 0; i < x; i++) {
+			for (let j = i; j < y - i; j++) {
+				const tmp = blocks[i][j]
+				if (clockWise) {
+					blocks[i][j] = blocks[y - j][i]
+					blocks[y - j][i] = blocks[y - i][y - j]
+					blocks[y - i][y - j] = blocks[j][y - i]
+					blocks[j][y - i] = tmp
+				} else {
+					blocks[i][j] = blocks[j][y - i]
+					blocks[j][y - i] = blocks[y - i][y - j]
+					blocks[y - i][y - j] = blocks[y - j][i]
+					blocks[y - j][i] = tmp
+				}
+			}
+		}
+	}
+
+	// поворот фигуры
+	hasCollision() {
+		const { y: pieceY, x: pieceX, blocks } = this.activePiece
+		const playfield = this.playfield
+
+		for (let y = 0; y < blocks.length; y++) {
+			for (let x = 0; x < blocks[y].length; x++) {
+				if (
+					blocks[y][x] &&
+					(playfield[pieceY + y] === undefined ||
+						playfield[pieceY + y][pieceX + x] === undefined ||
+						playfield[pieceY + y][pieceX + x])
+				) {
+					return true
+				}
+			}
+		}
+		return false
+	}
+
+	// фиксация фигуры в игровом поле
+	lockPiece() {
+		const { y: pieceY, x: pieceX, blocks } = this.activePiece
+
+		for (let y = 0; y < blocks.length; y++) {
+			for (let x = 0; x < blocks[y].length; x++) {
+				if (blocks[y][x]) {
+					this.playfield[pieceY + y][pieceX + x] = blocks[y][x]
+				}
+			}
+		}
+	}
+	clearLines() {
+		const rows = 20
+		const columns = 10
+		let lines = []
+
+		for (let y = rows - 1; y >= 0; y--) {
+			let numberOfBocks = 0
+
+			for (let x = 0; x < columns; x++) {
+				if (this.playfield[y][x]) {
+					numberOfBocks += 1
+				}
+			}
+			if (numberOfBocks === 0) {
+				break
+			} else if (numberOfBocks < columns) {
+				continue
+			} else if (numberOfBocks === columns) {
+				lines.unshift(y)
+			}
+		}
+		for (let index of lines) {
+			this.playfield.splice(index, 1)
+			this.playfield.unshift(new Array(columns).fill(0))
+		}
+		return lines.length
+	}
+
+	updateScore(clearedLines) {
+		if (clearedLines) {
+			this.score += Game.points[clearedLines] * (this.level + 1)
+			this.lines += clearedLines
+		}
+	}
+
+	updatePieces() {
+		this.activePiece = this.nextPiece
+		this.nextPiece = this.createPiece()
+	}
+}
